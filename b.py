82d90f50404a4e515e38b5426c709e03e0175963
chingfongsu <csu@polyvore.com>
2016-04-02 21:44:50 -0700

    b.9 from csu0402-3

--- b.py
+++ b.py
@@ -8,5 +8,10 @@ this is b.5 from csu0402-1
 this is b.6 from csu0402-2
 this is b.7 from csu0402-2
 
+<<<<<<< Upstream, based on origin/master
 this is b.8 from master for testting with csu0402-3
 this is b.9 from csu0402-3 for 
+=======
+this is b.8 from csu0402-3
+this is b.9 from csu0402-3
+>>>>>>> 3f27c7d b.9 from csu0402-3
